6f3bd2981883a7090e1e1a5dcb4aa1206849d778
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-02-03 14:09:04 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+'use client';
+import { motion } from 'framer-motion'
+import styles from '../styles';
+import { navVariants } from '../utils/motion'
+
+const Navbar = () => (
+  <motion.nav 
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    Navbar
+  </motion.nav>
+)
+export default Navbar;
