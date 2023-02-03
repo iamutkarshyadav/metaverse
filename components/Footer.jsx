2e4c2b3a1a473ac6a951aa0302ba5f153f05ab32
iamutkarshyadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-02-03 17:36:18 +0530

    main

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { socials } from "../constants";
+import { socials } from '../constants';
 
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
